ae87640e854e1b2789c207a8e86ec0d81640fb36
Derek Pockrandt <derekpock@gmail.com>
2023-07-28 02:53:11 -0500

    Add geometric math libraries and utilities

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,7 +1,33 @@
+import { checkIntersection } from 'line-intersect';
+
 export function defaultObjectValue(obj, key, defaultValue) {
     if (!obj.hasOwnProperty(key)) {
         obj[key] = defaultValue;
         return true;
     }
     return false;
+}
+
+/**
+ * @param {*} node Object with x, y
+ * @param {*} box Object with x1, y1, x2, y2
+ * @returns {boolean}
+ */
+export function doesPointIntersectBox(node, box) {
+    return (node.x > box.x1 && node.x < box.x2 && node.y > box.y1 && node.y < box.y2);
+}
+
+/**
+ * @param {*} node1 Object with x, y 
+ * @param {*} node2 Object with x, y
+ * @param {*} box Object with x1, y1, x2, y2
+ * @returns {boolean}
+ */
+export function doesLineIntersectBox(node1, node2, box) {
+    return doesPointIntersectBox(node1, box) ||
+        doesPointIntersectBox(node2, box) ||
+        checkIntersection(node1.x, node1.y, node2.x, node2.y, box.x1, box.y1, box.x2, box.y1).type === "intersecting" ||    // top
+        checkIntersection(node1.x, node1.y, node2.x, node2.y, box.x2, box.y1, box.x2, box.y2).type === "intersecting" ||    // right
+        checkIntersection(node1.x, node1.y, node2.x, node2.y, box.x2, box.y2, box.x1, box.y2).type === "intersecting" ||    // bottom
+        checkIntersection(node1.x, node1.y, node2.x, node2.y, box.x1, box.y2, box.x1, box.y1).type === "intersecting";      // left
 }
--- src/tests/Utilities.test.js
+++ src/tests/Utilities.test.js
@@ -1,20 +1,60 @@
-import { defaultObjectValue } from '../Utilities';
+import { defaultObjectValue, doesPointIntersectBox, doesLineIntersectBox } from '../Utilities';
+import { checkIntersection } from 'line-intersect';
 
-test("defaultObjectValue: no modification to existing key", () => {
-    let x = { myKey: "value" };
-    defaultObjectValue(x, "myKey", "newValue");
-    expect(x.myKey).toEqual("value");
+
+describe("defaultObjectValue", () => {
+    test("no modification to existing key", () => {
+        let x = { myKey: "value" };
+        defaultObjectValue(x, "myKey", "newValue");
+        expect(x.myKey).toEqual("value");
+    });
+    
+    test("adds non-existant key with default value", () => {
+        let x = { myKey: "value" };
+        defaultObjectValue(x, "myName", "newValue");
+        expect(x.myName).toEqual("newValue");
+    });
+    
+    test("adds non-existant key with array value", () => {
+        let x = { myKey: "value" };
+        defaultObjectValue(x, "myName", []);
+        expect(x.myName).toEqual([]);
+        expect(x.myName.length).toEqual(0);
+    });    
 });
 
-test("defaultObjectValue: adds non-existant key with default value", () => {
-    let x = { myKey: "value" };
-    defaultObjectValue(x, "myName", "newValue");
-    expect(x.myName).toEqual("newValue");
+describe("doesPointIntersectBox", () => {
+    test("point in box", () => {
+        expect(doesPointIntersectBox({x: 1, y: 1}, {x1: 0, y1: 0, x2: 2, y2: 2})).toEqual(true);
+    });
+
+    test.each([
+        [1, -1],    // north
+        [4, -1],    // north-east
+        [4, 1],     // east
+        [4, 4],     // south-east
+        [1, 4],     // south
+        [-1, 4],    // south-west
+        [-1, 1],    // west
+        [-1, -1]    // north-west
+    ])("point (%d, %d) outside of box", (paramX, paramY) => {
+        expect(doesPointIntersectBox({x: paramX, y: paramY}, {x1: 0, y1: 0, x2: 2, y2: 2})).toEqual(false);
+    });
 });
 
-test("defaultObjectValue: adds non-existant key with array value", () => {
-    let x = { myKey: "value" };
-    defaultObjectValue(x, "myName", []);
-    expect(x.myName).toEqual([]);
-    expect(x.myName.length).toEqual(0);
+describe("doesLineIntersectBox", () => {
+    test.each([
+        [1, 1, 10, 10],     // n1 in box
+        [10, 10, 1, 1],     // n2 in box
+        [1, -10, 1, 10],    // n to s
+        [10, -10, -10, 10], // ne to sw
+        [10, 1, -10, 1],    // e to w
+        [10, 10, -10, -10], // sw to ne
+        [1, 10, 1, -10],    // s to n
+        [-10, 10, 10, -10], // se to nw,
+        [-10, 1, 10, 1],    // w to e
+        [-10, -10, 10, 10]  // nw to se
+    ])("line from (%d, %d) to (%d, %d) intersects box", (x1, y1, x2, y2) => {
+        expect(doesLineIntersectBox({x: x1, y: y1}, {x: x2, y: y2}, {x1: -2, y1: -2, x2: 2, y2: 2})).toEqual(true);
+    });
 });
